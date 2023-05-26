e0d5f1f134d0cc532f4fc03f76bbe6b4ed6d0eb5
Farkhod Botirov <botirovfarhod115@gmail.com>
2023-05-26 11:17:56 +0500

    feat:add cdk

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,67 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, "JSCC-OAI");
+    const siteBucket = new s3.Bucket(this, "ReactShopBucket", {
+      bucketName: "react-cc-cloudfront-s3",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["S3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "JSCC-distribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+              originAccessIdentity: cloudfrontOAI,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    new s3Deploy.BucketDeployment(this, "JSCC-Bucket-Deployment", {
+      sources: [s3Deploy.Source.asset("../dist")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
