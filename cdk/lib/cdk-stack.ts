e921e3958405c29a35272ca96540f9a7aa3c046f
Farkhod Botirov <botirovfarhod115@gmail.com>
2023-05-28 22:44:37 +0500

    feat:add npm commands

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -3,16 +3,14 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
 import * as iam from "aws-cdk-lib/aws-iam";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront"; // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, "JSCC-OAI");
-    const siteBucket = new s3.Bucket(this, "ReactShopBucket", {
-      bucketName: "react-cc-cloudfront-s3",
+    const siteBucket = new s3.Bucket(this, "ReactShodBucket", {
+      bucketName: "react-cloudfront-s3",
       websiteIndexDocument: "index.html",
       publicReadAccess: false,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
@@ -56,7 +54,6 @@ export class CdkStack extends cdk.Stack {
       distribution,
       distributionPaths: ["/*"],
     });
-
     // The code that defines your stack goes here
 
     // example resource
