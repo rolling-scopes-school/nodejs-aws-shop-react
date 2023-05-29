80d16dd6f21c41a35a37e3855429c9a1def40bd8
Andrei Shamrei <Andrei.Shamrei@coxautoinc.com>
2023-05-29 20:35:20 +0400

    Task 2.2 - Add s3-website without access

--- cdk.ts
+++ cdk.ts
@@ -1,11 +1,9 @@
 import { App, Stack, CfnOutput } from "aws-cdk-lib";
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Bucket, BlockPublicAccess } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
-import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 import {
   OriginAccessIdentity,
-  Distribution,
-  ViewerProtocolPolicy,
+  CloudFrontWebDistribution,
 } from "aws-cdk-lib/aws-cloudfront";
 
 import {
@@ -13,34 +11,47 @@ import {
   BUCKET_NAME,
   DEFAULT_REGION,
   BUILD_FOLDER_PATH,
+  STACK_ID,
   BUCKET_ID,
   ORIGIN_AI_ID,
   DISTRIBUTION_ID,
   BUCKET_DEPLOYMENT_ID,
   CFN_OUTPUT_ID,
+  ENTRY_POINT,
 } from "./constants.js";
 
 const app = new App();
-const stack = new Stack(app, STACK_NAME, { env: { region: DEFAULT_REGION } });
-const bucket = new Bucket(stack, BUCKET_ID, { bucketName: BUCKET_NAME });
-
-const originAccessIdentity = new OriginAccessIdentity(stack, ORIGIN_AI_ID, {
-  comment: bucket.bucketName,
+const stack = new Stack(app, STACK_ID, {
+  stackName: STACK_NAME,
+  env: { region: DEFAULT_REGION },
+  description:
+    "The main Cloud Formation Stack to manage all resources of the Shop application",
 });
 
-bucket.grantRead(originAccessIdentity);
+const originAccessIdentity = new OriginAccessIdentity(stack, ORIGIN_AI_ID, {
+  comment: `Allows CloudFront to reach the bucket - ${BUCKET_NAME}`,
+});
 
-const cloudfront = new Distribution(stack, DISTRIBUTION_ID, {
-  defaultBehavior: {
-    origin: new S3Origin(bucket, { originAccessIdentity }),
-    viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-  },
-  defaultRootObject: "index.html",
-  errorResponses: [
+const bucket = new Bucket(stack, BUCKET_ID, {
+  bucketName: BUCKET_NAME,
+  websiteIndexDocument: ENTRY_POINT,
+  blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+});
+
+const distribution = new CloudFrontWebDistribution(stack, DISTRIBUTION_ID, {
+  comment: "Cloud Front Distribution for deploy Shop App",
+  defaultRootObject: ENTRY_POINT,
+  originConfigs: [
     {
-      httpStatus: 404,
-      responseHttpStatus: 200,
-      responsePagePath: "/index.html",
+      s3OriginSource: {
+        s3BucketSource: bucket,
+        originAccessIdentity,
+      },
+      behaviors: [
+        {
+          isDefaultBehavior: true,
+        },
+      ],
     },
   ],
 });
@@ -48,10 +59,11 @@ const cloudfront = new Distribution(stack, DISTRIBUTION_ID, {
 new BucketDeployment(stack, BUCKET_DEPLOYMENT_ID, {
   destinationBucket: bucket,
   sources: [Source.asset(BUILD_FOLDER_PATH)],
-  distribution: cloudfront,
+  distribution,
   distributionPaths: ["/*"],
 });
 
 new CfnOutput(stack, CFN_OUTPUT_ID, {
-  value: cloudfront.distributionDomainName,
+  description: "Cloud Front Deploy URL",
+  value: distribution.distributionDomainName,
 });
