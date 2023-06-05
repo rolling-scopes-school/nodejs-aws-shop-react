fcb0e2479fcfd7dba329c7700c20760cd5df8d8e
Peter Markushin <p.markushin@itransition.com>
2023-06-06 01:34:40 +0300

    Update API url to point to backend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://49x3bleq23.execute-api.us-east-1.amazonaws.com/prod/v1",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
