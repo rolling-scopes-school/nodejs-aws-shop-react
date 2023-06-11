26fdfa200c630ebaaa80e475a3a1a6c0761c0189
Mark Karnaukh <marik13021992@gmail.com>
2023-06-11 16:25:19 +0200

    Final changes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: 'https://odpq3kqzb7.execute-api.eu-central-1.amazonaws.com/dev/',
+  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://odpq3kqzb7.execute-api.eu-central-1.amazonaws.com/dev/',
