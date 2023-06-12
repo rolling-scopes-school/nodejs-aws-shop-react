a628a80043fcdcbd6aa80abee4ee9624d4228db3
barbedwire <e.barbedwire@gmail.com>
2023-06-12 14:53:54 +0200

    change paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,4 +1,5 @@
 const API_PATHS = {
+  base: "https://1lwpgiz791.execute-api.eu-west-1.amazonaws.com/",
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
