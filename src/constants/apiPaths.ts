226602e374077934a5ccea65136067e0c7f30ce0
Nikolai V. Tolochnyi <nikola-tolochny@mail.ru>
2023-06-18 15:30:45 +0400

    task-5: FE changes for import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://5sa7tml0b4.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://m1rcin07mg.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
