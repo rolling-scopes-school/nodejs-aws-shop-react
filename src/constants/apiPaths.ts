8d3a9407127f8671ea8da66c02aee249e2c84800
Andrei Shamrei <Andrei.Shamrei@coxautoinc.com>
2023-06-18 23:30:42 +0300

    Task 5 - Add integration with Import Service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://cglu0qwmn6.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://fb17o516yi.execute-api.eu-west-1.amazonaws.com/prod",
+  bff: "https://cglu0qwmn6.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
