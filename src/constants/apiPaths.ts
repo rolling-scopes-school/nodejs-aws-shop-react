dabd692742e714c40f233219dedd2049bfcf739f
Yana Bashtyk <yankabashtyk16@gmail.com>
2023-06-18 17:03:12 +0400

    Integrate lambda endpoint with frontend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://870nyoxs79.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://yn3vwidmu2.execute-api.eu-west-1.amazonaws.com",
   bff: "https://870nyoxs79.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
