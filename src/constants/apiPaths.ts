3205f6a81227f34342b65be2c6166e22f6c3259d
DaminikDavydau <Dominikdavydov3d@gmail.com>
2023-06-19 18:20:22 +0300

    fix: add correct import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://b99iy62bk9.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://1guehqyu0h.execute-api.eu-west-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
