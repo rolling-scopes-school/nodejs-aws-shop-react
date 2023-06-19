c88bc48b3f83fb9a652cfda4e760955e694699da
legankov95 <legankov95@gmail.com>
2023-06-19 23:55:27 +0400

    feat: resolve 5th task

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,13 @@
 const BASE_PATH =
   "https://bxap70ethc.execute-api.eu-west-1.amazonaws.com/prod/";
 
+const IMPORT_PATH =
+  "https://owxgrms5fl.execute-api.eu-west-1.amazonaws.com/prod/";
+
 const API_PATHS = {
   product: BASE_PATH + "products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: IMPORT_PATH + "import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
