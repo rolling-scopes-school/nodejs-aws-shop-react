a7c2e06ef2f259eb3b9fd991d6345af0eb00b66a
Mark Karnaukh <marik13021992@gmail.com>
2023-06-19 22:59:16 +0200

    Provide FE adjustments according to the Task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://858sy8vbrf.execute-api.eu-central-1.amazonaws.com/dev',
+  import: 'https://54fi7ai2x1.execute-api.eu-central-1.amazonaws.com/dev',
+  bff: 'https://kv2ye89kt3.execute-api.eu-central-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
