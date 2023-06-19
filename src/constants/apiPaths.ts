4f1907bd150f2e2b261f6119805b1db8001d037c
ShlemenKirill <shlemen_kirill@outlook.com>
2023-06-19 20:01:45 +0200

    import api integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://f8rvcdamz8.execute-api.eu-central-1.amazonaws.com",
   bff: "https://23cipbiokj.execute-api.eu-central-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
