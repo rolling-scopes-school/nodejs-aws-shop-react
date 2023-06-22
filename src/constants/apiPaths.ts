904800791dfba79fa3fc46de2db668d986994d47
Peter Markushin <p.markushin@itransition.com>
2023-06-22 15:38:21 +0300

    Add integration with product file import API endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://49x3bleq23.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://49x3bleq23.execute-api.us-east-1.amazonaws.com/prod/v1",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
