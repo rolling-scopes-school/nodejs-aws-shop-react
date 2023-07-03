8e772a9d75c547e2c4c08f53ffe30b50158554a1
Mark Karnaukh <marik13021992@gmail.com>
2023-07-03 21:35:20 +0200

    Update API Service URLs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://6s4e324cw5.execute-api.eu-central-1.amazonaws.com/dev',
-  bff: 'https://krfyej27ra.execute-api.eu-central-1.amazonaws.com/dev',
+  import: 'https://iiielb6s27.execute-api.eu-central-1.amazonaws.com/dev',
+  bff: 'https://wfvhgvxizg.execute-api.eu-central-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
