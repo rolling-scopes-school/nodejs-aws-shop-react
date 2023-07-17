806b4cf24682d7de4d32334f2af53189b891f57c
Nikolai V. Tolochnyi <nikola-tolochny@mail.ru>
2023-07-18 01:48:13 +0400

    task-9: update apiPaths with elasticbeanstalk link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://5sa7tml0b4.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://m1rcin07mg.execute-api.us-east-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://ckpuna4-cart-api-dev.us-east-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
--- src/queries/cart.ts
+++ src/queries/cart.ts
@@ -6,12 +6,16 @@ import { CartItem } from "~/models/CartItem";
 
 export function useCart() {
   return useQuery<CartItem[], AxiosError>("cart", async () => {
-    const res = await axios.get<CartItem[]>(`${API_PATHS.cart}/profile/cart`, {
+    const res = await axios.get<{
+      cart: {
+        items: CartItem[];
+      };
+    }>(`${API_PATHS.cart}/profile/cart`, {
       headers: {
         Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
       },
     });
-    return res.data;
+    return res.data.cart.items;
   });
 }
 
