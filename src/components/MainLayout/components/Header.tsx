359c26591af72a64789b3d8c5f1c09c348709ab7
alexej1900 <alexej1900@gmail.com>
2023-11-15 22:09:37 +0100

    edit: store title, header color

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   };
 
   return (
-    <AppBar position="relative">
+    <AppBar position="relative"  style={{ backgroundColor: '#00ff60' }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Book Store!
           </Link>
         </Typography>
 
