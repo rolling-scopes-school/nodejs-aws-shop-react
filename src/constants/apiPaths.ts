da34989b47fc360ee951813ba37e0ae1811c6555
Alex Zamirouski <alex.zamirouski@gmail.com>
2023-11-18 23:38:35 +0300

    products service integrated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://xv4rj7n3m7.execute-api.eu-north-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
