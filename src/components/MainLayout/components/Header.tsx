37ae2e8214427cb21f67be5889fd31c0dec08011
alexej1900 <alexej1900@gmail.com>
2023-11-18 18:35:16 +0100

    change: header color, mocks data

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   };
 
   return (
-    <AppBar position="relative"  style={{ backgroundColor: '#00ff60' }}>
+    <AppBar position="relative"  style={{ backgroundColor: '#03aa40' }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -8,37 +8,37 @@ export const products: Product[] = [
     description: "Short Product Description1",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
     price: 24,
-    title: "Python For Dummies",
+    title: "ProductOne",
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
     price: 15,
-    title: "Java For Dummies",
+    title: "ProductTitle",
   },
   {
     description: "Short Product Description2",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
     price: 23,
-    title: "JavaScript For Dummies",
+    title: "Product",
   },
   {
     description: "Short Product Description4",
     id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
     price: 15,
-    title: "C++ For Dummies",
+    title: "ProductTest",
   },
   {
     description: "Short Product Descriptio1",
     id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
     price: 23,
-    title: "C# For Dummies",
+    title: "Product2",
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
     price: 15,
-    title: "Ruby For Dummies",
+    title: "ProductName",
   },
 ];
 
