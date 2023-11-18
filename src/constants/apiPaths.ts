a05fbac59fa6a79428f58492ddeea6e144707ffe
Henadzi Shutko <hazardsoft@gmail.com>
2023-11-18 21:04:55 +0300

    fix: update products api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://coixf7w53g.execute-api.eu-central-1.amazonaws.com/dev",
+  product: "https://dw0r0hgmic.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
