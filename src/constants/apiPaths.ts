28e5198781d3b76720a428737e09041ae6a7f667
Alisher Mamunov <mamunovalisher@gmail.com>
2023-11-19 16:22:04 -0600

    feat: add api urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://87fldmfs19.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://87fldmfs19.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://87fldmfs19.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://87fldmfs19.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://87fldmfs19.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
