7dce3ac0daf9980e8301c4468c0eb13fc73bc419
Eduard Grigoryan <eduard_grigoryan@epam.com>
2023-11-20 19:20:54 +0400

    task done

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import * as cdk from '@aws-cdk/core';
+import { StaticSite } from './static-site';
 
 class MyStatisticStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string) {
     super(parent, name);
+
+    new StaticSite(this, 'first-app-aws');
   }
 }
 
