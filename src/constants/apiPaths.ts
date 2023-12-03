34ae034167f3b7410091de52b868437fabbab47f
Kate <ekaterina21051988@yandex.by>
2023-12-03 23:22:15 +0200

    feat: code and path have been updated according to task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://hjfant4yl8.execute-api.eu-west-1.amazonaws.com",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://hjfant4yl8.execute-api.eu-west-1.amazonaws.com",
+  import: "https://ocyeq7jh0j.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://hjfant4yl8.execute-api.eu-west-1.amazonaws.com",
+  cart: "https://hjfant4yl8.execute-api.eu-west-1.amazonaws.com",
 };
 
 export default API_PATHS;
