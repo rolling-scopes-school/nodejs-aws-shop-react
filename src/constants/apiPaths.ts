626c4d0077d02ffdbca449cb4ac970427828afc1
hs-himzheuski <vitali.himzheuski@headspace.com>
2023-12-16 18:47:46 +0100

    update path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://hy98va2hj9.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://ge9tn7uc45.execute-api.eu-west-1.amazonaws.com/prod",
+  import: "https://j1hl0ceqk0.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
