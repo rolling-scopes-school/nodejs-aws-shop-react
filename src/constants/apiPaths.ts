1aef994db8e51d2d7caa0d12c758de7bc71eaf01
alexej1900 <alexej1900@gmail.com>
2023-12-18 21:09:25 +0100

    change: import link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://nwt9l201g9.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://3l51fpxvd5.execute-api.eu-west-1.amazonaws.com/prod/",
+  import: "https://3l51fpxvd5.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://nwt9l201g9.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
