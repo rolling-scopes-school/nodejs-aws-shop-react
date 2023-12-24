cf7f613b872a5a85e4be56e3d9613bbe3484680d
alexej1900 <alexej1900@gmail.com>
2023-12-24 17:04:59 +0100

    add: order link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product: "https://nwt9l201g9.execute-api.eu-west-1.amazonaws.com",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://iaje6cks9g.execute-api.eu-west-1.amazonaws.com",
   import: "https://anl9jzxk4k.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://nwt9l201g9.execute-api.eu-west-1.amazonaws.com",
   cart: "https://iaje6cks9g.execute-api.eu-west-1.amazonaws.com",
