bb7cc379b2b10a589ebe60131f77a4526f5104e8
Aliaksandr Zamirouski <alex.zamirouski@gmail.com>
2023-12-25 11:56:54 +0300

    task_8 api gateway integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://xv4rj7n3m7.execute-api.eu-north-1.amazonaws.com",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://qxrkvihds6.execute-api.eu-north-1.amazonaws.com",
   import: "https://1cixh8npm5.execute-api.eu-north-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://qxrkvihds6.execute-api.eu-north-1.amazonaws.com",
 };
 
 export default API_PATHS;
