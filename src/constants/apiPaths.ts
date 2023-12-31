b62cbb3d5300a7f36ce51a9b637893f3fbcf09e3
Yauhen Kulevich <latvelsit@gmail.com>
2023-12-31 18:52:27 +0300

    change paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://obb0c59ll3.execute-api.eu-west-1.amazonaws.com/prod",
-  order: "http://latvels-cart-api-r.eba-r9p9rdn9.us-west-2.elasticbeanstalk.com",
+  order: "http://latvels-cart-api-re.eba-r9p9rdn9.us-west-2.elasticbeanstalk.com",
   import: "https://ipp0y4h1fg.execute-api.eu-west-1.amazonaws.com",
   bff: "https://obb0c59ll3.execute-api.eu-west-1.amazonaws.com/prod",
-  cart: "http://latvels-cart-api-r.eba-r9p9rdn9.us-west-2.elasticbeanstalk.com",
+  cart: "http://latvels-cart-api-re.eba-r9p9rdn9.us-west-2.elasticbeanstalk.com/",
 };
 
 export default API_PATHS;
