3df78c704299f6536ca5ea01a50c6b403dbcc059
Pashtet <ptashkaaaaaa@gmail.com>
2024-06-06 15:20:26 +0200

    feat: create aws app, add bucket creation, add policy to bucket, create distribution for cloudfront, add s3 deploy

---
+++ aws_cdk/lib/aws_cdk-stack.ts
@@ -0,0 +1,65 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  aws_s3 as s3,
+  aws_iam as iam,
+  aws_cloudfront as cloudfront,
+  aws_s3_deployment as s3deploy,
+} from "aws-cdk-lib";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(
+      this,
+      "RsshoolCloudFrontOAI",
+      {
+        comment: "OAI for my distribution",
+      }
+    );
+
+    const siteBucket = new s3.Bucket(this, "RsshoolSiteBucket", {
+      bucketName: "rsshool-site-bucket",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+        actions: ["s3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+      })
+    );
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "RsshoolCloudFrontDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+              originAccessIdentity: cloudFrontOAI,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+
+    new s3deploy.BucketDeployment(this, "RsshoolBucketDeployment", {
+      sources: [s3deploy.Source.asset("../dist")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+  }
+}
