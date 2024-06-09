3fb6b8844e8753157f6596b861251b412d6c9b87
Ilya Basiuk <ibasiuk2@slb.com>
2024-06-09 14:19:37 +0200

    cdk: add and configure S3 bucket creation and website deployment

--- cdk-deployment/lib/cdk-deployment-stack.ts
+++ cdk-deployment/lib/cdk-deployment-stack.ts
@@ -1,5 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkDeploymentStack extends cdk.Stack {
@@ -8,6 +10,25 @@ export class CdkDeploymentStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    const bucket = new s3.Bucket(this, "MyShopBucket", {
+      bucketName: "nodejs-aws-shop-react-cdk-2",
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      },
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("../dist")],
+      destinationBucket: bucket,
+    });
     // example resource
     // const queue = new sqs.Queue(this, 'CdkDeploymentQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
