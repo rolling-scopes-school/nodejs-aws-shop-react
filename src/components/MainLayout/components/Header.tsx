baddbaf548bd948a3756fdd8195f2719b195efcb
Liudmila Arzamasova <liudmila_arzamasava@epam.com>
2024-06-10 02:42:20 +0300

    task 2 completed

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My Store! Task-2
           </Link>
         </Typography>
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,13 @@
 import { createTheme } from "@mui/material/styles";
+import { green, blue } from "@mui/material/colors";
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[200],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
