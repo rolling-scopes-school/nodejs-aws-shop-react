41f8a91345209bd53400b5b8f2f934488f9a2c1e
max <mcnic@users.noreply.github.com>
2024-06-09 15:05:14 -0700

    feat: add S3 Bucket

--- aws_cdk/lib/aws_cdk-stack.ts
+++ aws_cdk/lib/aws_cdk-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { aws_s3 as s3 } from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import { Stack, StackProps, RemovalPolicy } from "aws-cdk-lib/";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myBucket = new Bucket(this, "MyBucket", {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
   }
 }
