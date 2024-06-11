1e825708e7c2db24f44bc0a00bd7d7f54ac7dd3c
max <mcnic@users.noreply.github.com>
2024-06-11 15:11:15 -0700

    fix: remove commented code

--- aws_cdk/construct/task02.ts
+++ aws_cdk/construct/task02.ts
@@ -1,36 +1,20 @@
 #!/usr/bin/env node
-// import * as route53 from "aws-cdk-lib/aws-route53";
 import * as s3 from "aws-cdk-lib/aws-s3";
-// import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
-// import * as targets from "aws-cdk-lib/aws-route53-targets";
 import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
 import { Duration, RemovalPolicy, Stack } from "aws-cdk-lib";
 import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
-// import path = require("path");
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface StaticSiteProps {
-  // domainName: string;
-  // siteSubDomain: string;
-}
 
 /**
  * Static site infrastructure, which deploys site content to an S3 bucket.
- *
- * The site redirects from HTTP to HTTPS, using a CloudFront distribution,
- * Route53 alias record, and ACM certificate is commented
+ * full code on https://github.com/aws-samples/aws-cdk-examples/blob/main/typescript/static-site/static-site.ts
  */
-export class Task02 extends Construct {
-  constructor(parent: Stack, name: string /* , props: StaticSiteProps */) {
-    super(parent, name);
 
-    // const zone = route53.HostedZone.fromLookup(this, "Zone", {
-    //   domainName: props.domainName,
-    // });
-    // const siteDomain = props.siteSubDomain + "." + props.domainName;
+export class Task02 extends Construct {
+  constructor(parent: Stack, name: string) {
+    super(parent, name);
 
     const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
       this,
@@ -40,25 +24,13 @@ export class Task02 extends Construct {
       }
     );
 
-    // new CfnOutput(this, "Site", { value: "https://" + siteDomain });
-
     // Content bucket
     const siteBucket = new s3.Bucket(this, "SiteBucket", {
-      // bucketName: siteDomain,
       publicReadAccess: false,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
 
-      /**
-       * The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
-       * the new bucket, and it will remain in your account until manually deleted. By setting the policy to
-       * DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
-       */
       removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
 
-      /**
-       * For sample purposes only, if you create an S3 bucket then populate it, stack destruction fails.  This
-       * setting will enable full cleanup of the demo.
-       */
       autoDeleteObjects: true, // NOT recommended for production code
     });
 
@@ -74,20 +46,10 @@ export class Task02 extends Construct {
         ],
       })
     );
-    // new CfnOutput(this, "Bucket", { value: siteBucket.bucketName });
-
-    // TLS certificate
-    // const certificate = new acm.Certificate(this, "SiteCertificate", {
-    //   domainName: siteDomain,
-    //   validation: acm.CertificateValidation.fromDns(zone),
-    // });
-    // new CfnOutput(this, "Certificate", { value: certificate.certificateArn });
 
     // CloudFront distribution
     const distribution = new cloudfront.Distribution(this, "SiteDistribution", {
-      // certificate: certificate,
       defaultRootObject: "index.html",
-      // domainNames: [siteDomain],
       minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
       errorResponses: [
         {
@@ -106,19 +68,6 @@ export class Task02 extends Construct {
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
     });
-
-    // new CfnOutput(this, "DistributionId", {
-    //   value: distribution.distributionId,
-    // });
-
-    // Route53 alias record for the CloudFront distribution
-    // new route53.ARecord(this, "SiteAliasRecord", {
-    //   recordName: siteDomain,
-    //   target: route53.RecordTarget.fromAlias(
-    //     new targets.CloudFrontTarget(distribution)
-    //   ),
-    //   zone,
-    // });
 
     // Deploy site contents to S3 bucket
     new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
