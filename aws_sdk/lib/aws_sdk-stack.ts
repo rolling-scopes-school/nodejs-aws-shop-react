0d7793fa708915d60e64f647c0aa1be5e951072a
Elizaveta Razumenko <elizaveta.razumenko@pargoo.com>
2024-06-11 12:11:00 +0300

    fix code according review

--- aws_sdk/lib/aws_sdk-stack.ts
+++ aws_sdk/lib/aws_sdk-stack.ts
@@ -4,7 +4,6 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsSdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
--- invalidate-cache.ts
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-const stackName = 'AwsSdkStack';
-const outputKey = 'DistributionId';
-
-const distributionId = execSync(`aws cloudformation describe-stacks --stack-name ${stackName} --query "Stacks[0].Outputs[?OutputKey=='${outputKey}'].OutputValue" --output text`).toString().trim();
-
-execSync(`aws cloudfront create-invalidation --distribution-id ${distributionId} --paths '/*'`, { stdio: 'inherit' });
