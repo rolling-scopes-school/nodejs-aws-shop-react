532fc9d25d400c5b6b37cd8f0b93bbaeb8c908d6
max <mcnic@users.noreply.github.com>
2024-06-11 15:13:19 -0700

    fix: remove unused comments

--- aws_cdk/lib/aws_cdk-stack.ts
+++ aws_cdk/lib/aws_cdk-stack.ts
@@ -6,18 +6,6 @@ export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    /**
-     * This stack relies on getting the domain name from CDK context.
-     * Use 'cdk synth -c domain=mystaticsite.com -c subdomain=www'
-     * Or add the following to cdk.json:
-     * {
-     *   "context": {
-     *     "domain": "mystaticsite.com",
-     *     "subdomain": "www",
-     *     "accountId": "1234567890",
-     *   }
-     * }
-     **/
     new Task02(this, "StaticSite");
   }
 }
