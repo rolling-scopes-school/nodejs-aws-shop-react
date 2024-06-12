6c1ccaa8ea76a25f27af78bfb852f82fdc2443b9
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-06-12 12:04:23 +0200

    feat: integrate fe app with product-service

--- src/queries/products.ts
+++ src/queries/products.ts
@@ -9,8 +9,9 @@ export function useAvailableProducts() {
     "available-products",
     async () => {
       const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.bff}/product/available`
+        `${API_PATHS.product}/products`
       );
+      console.log(res.data);
       return res.data;
     }
   );
@@ -29,7 +30,7 @@ export function useAvailableProduct(id?: string) {
     ["product", { id }],
     async () => {
       const res = await axios.get<AvailableProduct>(
-        `${API_PATHS.bff}/product/${id}`
+        `${API_PATHS.product}/products/${id}`
       );
       return res.data;
     },
