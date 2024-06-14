ae6be55c88d7cb6120b871978c46f4d1bf37952f
gerrkoff <antonkofa@gmail.com>
2024-06-15 01:40:37 +0200

    task-3b

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   // product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  product: "https://4glg577vhj.execute-api.eu-central-1.amazonaws.com",
+  product: "https://bct3g511jj.execute-api.eu-central-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
