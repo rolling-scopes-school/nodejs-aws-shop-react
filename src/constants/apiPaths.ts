04bb3b6e63cf8d0e97d1db119dfb38d5c00cd05a
max <mcnic@users.noreply.github.com>
2024-06-15 13:50:24 -0700

    feat: change API url to mocked backend aws API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,8 @@
 const prefix = import.meta.env.VITE_API_PREFIX || ".execute-api2";
+const awsAPI = "https://sm7jmgfua1.execute-api.us-east-1.amazonaws.com/prod";
 
 const API_PATHS = {
-  product: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
+  product: awsAPI,
   order: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
   import: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
   bff: `https://${prefix}.eu-west-1.amazonaws.com/dev`,
