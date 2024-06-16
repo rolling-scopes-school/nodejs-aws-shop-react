ace36f1360248bd35ac41f78433f0ffe232e93bf
Bektur777 <ubei003@mail.ru>
2024-06-16 22:20:50 +0600

    Fix features

--- src/queries/products.ts
+++ src/queries/products.ts
@@ -9,7 +9,7 @@ export function useAvailableProducts() {
     "available-products",
     async () => {
       const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.product}/product/available`
+        `${API_PATHS.product}/products`
       );
       return res.data;
     }
@@ -29,7 +29,7 @@ export function useAvailableProduct(id?: string) {
     ["product", { id }],
     async () => {
       const res = await axios.get<AvailableProduct>(
-        `${API_PATHS.product}/product/${id}`
+        `${API_PATHS.product}/products/${id}`
       );
       return res.data;
     },
