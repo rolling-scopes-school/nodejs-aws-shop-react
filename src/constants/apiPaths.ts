8579ce589b961fa95313231ac81d80c47928f2c4
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-06-19 18:58:13 +0200

    update post request

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://npjkcn07fi.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://871xbpugrg.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
