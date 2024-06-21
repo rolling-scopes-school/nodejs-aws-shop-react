c39d5f1973a95deaea820d7c992469717190ca76
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-06-21 15:19:49 +0200

    feat: add product images

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -7,6 +7,7 @@ import Typography from "@mui/material/Typography";
 import { formatAsPrice } from "~/utils/utils";
 import AddProductToCart from "~/components/AddProductToCart/AddProductToCart";
 import { useAvailableProducts } from "~/queries/products";
+import imagesData from "~/images/data.json";
 
 export default function Products() {
   const { data = [], isLoading } = useAvailableProducts();
@@ -18,28 +19,31 @@ export default function Products() {
   return (
     <Grid container spacing={4}>
       {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-      {data.map(({ count, ...product }, index) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4}>
-          <Card
-            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
-          >
-            <CardMedia
-              sx={{ pt: "56.25%" }}
-              image={`https://source.unsplash.com/random?sig=${index}`}
-              title="Image title"
-            />
-            <CardContent sx={{ flexGrow: 1 }}>
-              <Typography gutterBottom variant="h5" component="h2">
-                {product.title}
-              </Typography>
-              <Typography>{formatAsPrice(product.price)}</Typography>
-            </CardContent>
-            <CardActions>
-              <AddProductToCart product={product} />
-            </CardActions>
-          </Card>
-        </Grid>
-      ))}
+      {data.map(({ count, ...product }, index) => {
+        const img = imagesData.find((img) => img.product_id === product.id);
+        return (
+          <Grid item key={product.id} xs={12} sm={6} md={4}>
+            <Card
+              sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+            >
+              <CardMedia
+                sx={{ pt: "56.25%" }}
+                image={img?.source}
+                title="Image title"
+              />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {product.title}
+                </Typography>
+                <Typography>{formatAsPrice(product.price)}</Typography>
+              </CardContent>
+              <CardActions>
+                <AddProductToCart product={product} />
+              </CardActions>
+            </Card>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 }
