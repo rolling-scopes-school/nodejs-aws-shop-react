5f1f102e96b188753c5757ea4d5de5817bcd3799
Ilya Basiuk <ibasiuk2@slb.com>
2024-07-01 00:19:48 +0200

    task-5 import file integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://dyxjm4tjv9.execute-api.eu-west-1.amazonaws.com/prod",
+  product: "https://7zu57pqm72.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://kvyj8o0ch7.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
