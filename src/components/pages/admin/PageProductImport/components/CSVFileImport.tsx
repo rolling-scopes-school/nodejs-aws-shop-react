3e01cb338d9dd061062e1278713e77a58fc8ec40
IharTsykala <ihatsykala@kitrum.com>
2024-06-30 21:15:46 +0500

    fix: inport route ans upload method[task-5-integration-s3]

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import axios from "axios";
 
 type CSVFileImportProps = {
   url: string;
@@ -25,23 +26,30 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const uploadFile = async () => {
     console.log("uploadFile to", url);
 
-    // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
+    if (file) {
+      const response = await axios({
+        method: "GET",
+        url,
+        params: {
+          name: encodeURIComponent(file.name),
+        },
+      });
+      console.log("File was uploaded: ", file.name);
+      console.log("Uploading to: ", response.data.url); // Изменено здесь
+
+      const result = await fetch(response.data.url, { // Изменено здесь
+        method: "PUT",
+        body: file,
+      });
+
+      console.log("File to upload: ", file?.name);
+      console.log("Result: ", result);
+      setFile(undefined);
+    } else {
+      console.log("error while uploading, file is undefined");
+    }
   };
+
   return (
     <Box>
       <Typography variant="h6" gutterBottom>
