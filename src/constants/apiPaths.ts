d329df7c0a2629ffe2b2c22969fb27bba754403d
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-07-01 02:26:15 +0300

    task5 integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://29rmxjt9qf.execute-api.eu-west-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://6bgkzg1yx3.execute-api.eu-west-1.amazonaws.com/prod/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
