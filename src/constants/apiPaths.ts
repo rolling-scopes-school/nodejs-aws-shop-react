d5fa50c83b4d77c398254bdd2535c8adcc1c5325
Aliaksei Tsvirko <aliaksei.tsvirko@hycom.pl>
2024-06-30 13:17:52 +0200

    Task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://3bjgkzg99e.execute-api.eu-west-1.amazonaws.com",
+  order: "https://3bjgkzg99e.execute-api.eu-west-1.amazonaws.com",
+  import: "https://3bjgkzg99e.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://3bjgkzg99e.execute-api.eu-west-1.amazonaws.com",
+  cart: "https://3bjgkzg99e.execute-api.eu-west-1.amazonaws.com",
 };
 
 export default API_PATHS;
