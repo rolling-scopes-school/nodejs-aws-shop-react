50361e34825217435412402062afdd6b9453c2ae
Elizaveta Razumenko <elizaveta.razumenko@pargoo.com>
2024-07-01 11:40:46 +0300

    add import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://9xtyq75i11.execute-api.eu-central-1.amazonaws.com/prod/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   products: "https://zo02svx37l.execute-api.eu-central-1.amazonaws.com/prod/",
