87485cf41b76833221048355e2f5502c5dccee42
IharTsykala <ihatsykala@kitrum.com>
2024-07-08 06:20:42 +0500

    fix: link to product stack and csv contant [task-6]

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://vzcpr84k64.execute-api.eu-central-1.amazonaws.com/prod/",
+  import: "https://saselr9wi3.execute-api.eu-central-1.amazonaws.com/prod/",
   bff: "https://ul03mg0kb5.execute-api.eu-central-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
