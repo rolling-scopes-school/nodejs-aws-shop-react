d9a5fc06f38ab14e5c4701046eef7c0842358c9f
Liliia Khannanova <98351208+likhanna@users.noreply.github.com>
2024-07-14 20:33:39 +0500

    task-5: Integration with S3 (#3)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://1r4bcu3kqf.execute-api.eu-west-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://pqk7o5l18l.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
