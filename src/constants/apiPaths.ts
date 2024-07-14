26ae1d16f9094abcccc32165a4a34b0f8196e576
IharTsykala <ihatsykala@kitrum.com>
2024-07-15 02:31:49 +0500

    feat: authorization-by-import[task-7]

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,3 +1,19 @@
+import axios from "axios";
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const statusResponse = error?.response?.status;
+    if (statusResponse === 401 || statusResponse === 403) {
+      alert(`Status ${statusResponse} ${error?.response?.data?.message}`);
+    }
+
+    return Promise.reject(error.response);
+  }
+);
+
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
