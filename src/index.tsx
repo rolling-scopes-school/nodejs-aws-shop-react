c198913029938c517d38fe9bff8fff9172302950
sneguroma <sneguroma@gmail.com>
2024-07-15 20:59:17 +0200

    feat: add alerts for the responses

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,22 @@ if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
 }
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        alert("Unauthorized! Please check your credentials.");
+      } else if (error.response.status === 403) {
+        alert("Forbidden! You do not have permission to access this resource.");
+      }
+    } else {
+      alert("Network error or the server is not responding.");
+    }
+    return Promise.reject(error);
+  }
+);
 
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
