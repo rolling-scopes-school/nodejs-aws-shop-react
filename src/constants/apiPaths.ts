f14f0b2b322208360a830823743d600e9b820fbc
Shriz07 <adamosmakiewicz7@gmail.com>
2024-07-16 19:22:06 +0200

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://de01ziaid1.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://o8ib9uipxg.execute-api.eu-north-1.amazonaws.com/prod",
+  bff: "https://de01ziaid1.execute-api.eu-north-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
