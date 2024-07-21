397d133e51aa9f783eaf791d2abcf56441676c62
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-07-21 12:33:31 +0200

    Add Cart API path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://v5j20r9hvl.execute-api.eu-north-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://vkof72vds7.execute-api.eu-north-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
