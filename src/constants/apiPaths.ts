b29bde21f51e900d5cfe1a69c1c070ccbc07eaef
Artem Lobovskii <lobovskiy@yandex.ru>
2024-07-25 17:16:21 +0300

    feat: add url of api gateway - proxy to elastic beanstalk - as a cart service api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://od2fzpzaa0.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "https://g7ktghs1ah.execute-api.eu-central-1.amazonaws.com/dev/api",
+  order: "https://7i991sybsd.execute-api.eu-central-1.amazonaws.com/dev/api",
   import: "https://nsr51h1em5.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://g7ktghs1ah.execute-api.eu-central-1.amazonaws.com/dev/api",
+  cart: "https://7i991sybsd.execute-api.eu-central-1.amazonaws.com/dev/api",
 };
 
 export default API_PATHS;
