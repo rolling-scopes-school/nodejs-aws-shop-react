c6b7d4e60d58557f344557489e1bcbdee9f4128e
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-08-05 21:21:01 +0200

    feat: update path to cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://ntvgqam3dl.execute-api.eu-west-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://roiers-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
