af682d389ae83ac88bbd8027be50a60acc5538d9
IharTsykala <ihatsykala@kitrum.com>
2024-08-05 11:59:12 +0500

    feat: add elasticbeanstalk link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -19,7 +19,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://saselr9wi3.execute-api.eu-central-1.amazonaws.com/prod/",
   bff: "https://ul03mg0kb5.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://ihartsykala-cart-api-develop.us-west-2.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
