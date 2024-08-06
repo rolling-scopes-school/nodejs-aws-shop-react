9e740da78dad64c1da2ef3181733de0b8f6e7560
Elizaveta Razumenko <elizaveta.razumenko@pargoo.com>
2024-08-07 00:57:24 +0300

    update the cart route

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://9xtyq75i11.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://elizavetarazumenko-cart-api-prod-elizaveta.eu-central-1.elasticbeanstalk.com/api",
+  cart: "https://os1v3hdadk.execute-api.eu-central-1.amazonaws.com/api",
   products: "https://zo02svx37l.execute-api.eu-central-1.amazonaws.com/prod",
 };
 
