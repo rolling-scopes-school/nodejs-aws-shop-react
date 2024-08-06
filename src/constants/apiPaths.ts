8959fbd8174879bc21e39761597af62653722837
Elizaveta Razumenko <elizaveta.razumenko@pargoo.com>
2024-08-07 00:12:46 +0300

    change the api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://9xtyq75i11.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://mgwj5xdzc5.execute-api.eu-central-1.amazonaws.com/prod/api",
+  cart: "http://elizavetarazumenko-cart-api-prod-elizaveta.eu-central-1.elasticbeanstalk.com/api",
   products: "https://zo02svx37l.execute-api.eu-central-1.amazonaws.com/prod",
 };
 
