fc1b31eb9e04f4f09d54cd2dfd3e97687a54ed07
Ivan Zakharanka <ivanzakharanka@gmail.com>
2024-10-22 19:01:00 +0200

    feat: add aws-cdk deploy

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,12 @@
 import { createTheme } from "@mui/material/styles";
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fcba03",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
