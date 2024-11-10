a57391da4da0c69c62c31926410de7404d145e4b
Ivan Zakharanka <ivanzakharanka@gmail.com>
2024-11-10 11:40:04 +0100

    feat: implement task-6

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://ztdr7sralb.execute-api.eu-west-1.amazonaws.com",
+  product: "https://40b23f3b4m.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://y7lo7x5j28.execute-api.eu-west-1.amazonaws.com",
-  bff: "https://ztdr7sralb.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://40b23f3b4m.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
