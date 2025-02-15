037ac5f8ab6794fa314f006e2cc8d1bdf93e46fb
Demion La <pradovd4@gmail.com>
2025-02-15 10:40:38 +0200

    MWS logic updated

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,4 @@
-import { rest } from "msw";
+import { http, HttpResponse, delay } from "msw";
 import API_PATHS from "~/constants/apiPaths";
 import { availableProducts, orders, products, cart } from "~/mocks/data";
 import { CartItem } from "~/models/CartItem";
@@ -6,56 +6,59 @@ import { Order } from "~/models/Order";
 import { AvailableProduct, Product } from "~/models/Product";
 
 export const handlers = [
-  rest.get(`${API_PATHS.bff}/product`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.delay(), ctx.json<Product[]>(products));
+
+  //Product endpoints
+  http.get(`${API_PATHS.bff}/product`, async () => {
+    await delay();
+    return HttpResponse.json(products);
   }),
-  rest.put(`${API_PATHS.bff}/product`, (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.put(`${API_PATHS.bff}/product`, async () => {
+    return new HttpResponse(null, { status: 200 });
   }),
-  rest.delete(`${API_PATHS.bff}/product/:id`, (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.delete(`${API_PATHS.bff}/product/:id`, async () => {
+    return new HttpResponse(null, { status: 200 });
   }),
-  rest.get(`${API_PATHS.bff}/product/available`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.delay(),
-      ctx.json<AvailableProduct[]>(availableProducts)
-    );
+  http.get(`${API_PATHS.bff}/product/available`, async () => {
+    await delay();
+    return HttpResponse.json(availableProducts);
   }),
-  rest.get(`${API_PATHS.bff}/product/:id`, (req, res, ctx) => {
-    const product = availableProducts.find((p) => p.id === req.params.id);
+
+  http.get(`${API_PATHS.bff}/product/:id`, async ({ params }) => {
+   const product = availableProducts.find((p) => p.id === params.id);
     if (!product) {
-      return res(ctx.status(404));
+      return new HttpResponse(null, { status: 404 });
     }
-    return res(
-      ctx.status(200),
-      ctx.delay(),
-      ctx.json<AvailableProduct>(product)
-    );
+    await delay();
+    return HttpResponse.json(product);
   }),
-  rest.get(`${API_PATHS.cart}/profile/cart`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.delay(), ctx.json<CartItem[]>(cart));
+
+  // Cart endpoints
+  http.get(`${API_PATHS.cart}/profile/cart`, async () => {
+    delay();
+    return HttpResponse.json(cart);
   }),
-  rest.put(`${API_PATHS.cart}/profile/cart`, (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.put(`${API_PATHS.cart}/profile/cart`, async () => {
+    return new HttpResponse(null, { status: 200 });
   }),
-  rest.get(`${API_PATHS.order}/order`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.delay(), ctx.json<Order[]>(orders));
+  http.get(`${API_PATHS.order}/order`, async () => {
+    await delay()
+    return HttpResponse.json(orders)
   }),
-  rest.put(`${API_PATHS.order}/order`, (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.put(`${API_PATHS.order}/order`, async () => {
+    return new HttpResponse(null, { status: 200 });
   }),
-  rest.get(`${API_PATHS.order}/order/:id`, (req, res, ctx) => {
-    const order = orders.find((p) => p.id === req.params.id);
+  http.get(`${API_PATHS.order}/order/:id`, async({ params }) => {
+    const order = orders.find((p) => p.id === params.id);
     if (!order) {
-      return res(ctx.status(404));
+      return new HttpResponse(null, { status: 404 });
     }
-    return res(ctx.status(200), ctx.delay(), ctx.json(order));
+    await delay();
+    return HttpResponse.json(order);
   }),
-  rest.delete(`${API_PATHS.order}/order/:id`, (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.delete(`${API_PATHS.order}/order/:id`, async () => {
+    return new HttpResponse(null, { status: 200 });
   }),
-  rest.put(`${API_PATHS.order}/order/:id/status`, (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.put(`${API_PATHS.order}/order/:id/status`, () => {
+    return new HttpResponse(null, { status: 200 });
   }),
 ];
