35781b3f5e7ade78826a59caca5b83b1c00a9d54
Ihor Osadchyi <ihor_osadchyi@epam.com>
2025-02-16 12:06:01 +0200

    feat(task_2): add cloudfront distribution

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class RsNodeAwsShop extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,20 +13,36 @@ export class RsNodeAwsShop extends cdk.Stack {
       bucketName: "rs-node-aws-shop-website-bucket",
       websiteIndexDocument: "index.html",
       websiteErrorDocument: "error.html",
-      publicReadAccess: true,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
+    const distribution = new cloudfront.Distribution(
+      this,
+      "WebsiteDistribution",
+      {
+        defaultBehavior: {
+          origin:
+            origins.S3BucketOrigin.withOriginAccessIdentity(websiteBucket),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        defaultRootObject: "index.html",
+      }
+    );
+
     new s3deploy.BucketDeployment(this, "DeployWebsite", {
       sources: [s3deploy.Source.asset("../dist")],
       destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ["/*"],
     });
 
-    new cdk.CfnOutput(this, "WebsiteURL", {
-      value: websiteBucket.bucketWebsiteUrl,
-      description: "The URL of the deployed website",
+    new cdk.CfnOutput(this, "CloudFrontURL", {
+      value: distribution.distributionDomainName,
+      description: "The CloudFront distribution URL for the website",
     });
   }
 }
