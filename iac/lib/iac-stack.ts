6fa6a3990090e66e8a4867d2357e38378db5f6f6
Ihor Osadchyi <ihor_osadchyi@epam.com>
2025-02-16 11:52:12 +0200

    feat(task_2): add s3 static deploymet

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -1,8 +1,30 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 
 export class RsNodeAwsShop extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      bucketName: "rs-node-aws-shop-website-bucket",
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "error.html",
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("../dist")],
+      destinationBucket: websiteBucket,
+    });
+
+    new cdk.CfnOutput(this, "WebsiteURL", {
+      value: websiteBucket.bucketWebsiteUrl,
+      description: "The URL of the deployed website",
+    });
   }
 }
