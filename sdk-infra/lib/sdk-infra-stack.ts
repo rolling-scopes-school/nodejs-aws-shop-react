903668a9fabad5dcfe8f4345fb352a26171e1f9c
natenadze1102 <natenadze1102@gmail.com>
2025-02-17 23:44:11 +0400

    hotfix/change websiteBucket props

--- sdk-infra/lib/sdk-infra-stack.ts
+++ sdk-infra/lib/sdk-infra-stack.ts
@@ -1,35 +1,44 @@
-// lib/sdk-infra-stack.ts
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { BlockPublicAccess, Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
+import { BlockPublicAccess, Bucket, BucketPolicy } from 'aws-cdk-lib/aws-s3';
 import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
-import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { S3BucketOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class SdkInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // 1. Create S3 Bucket (disable public read - we use CloudFront)
+    // 1. Create a PRIVATE S3 Bucket for website hosting
     const websiteBucket = new Bucket(this, 'Task2Bucket', {
-      websiteIndexDocument: 'index.html',
-
-      publicReadAccess: true,
-
-      blockPublicAccess: new BlockPublicAccess({
-        blockPublicAcls: false,
-        ignorePublicAcls: false,
-        blockPublicPolicy: false,
-        restrictPublicBuckets: false,
-      }),
-      websiteErrorDocument: 'index.html',
+      // websiteIndexDocument: 'index.html',
+      // websiteErrorDocument: 'index.html', because of this ot says Static Website that is not deisred behavior
+      publicReadAccess: false, // Ensure bucket is private
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // Block all public access
+      removalPolicy: RemovalPolicy.RETAIN, // Change as needed
     });
 
-    // 2. CloudFront OAI (grants CF read access to bucket)
+    // 2. Create CloudFront Origin Access Identity (OAI)
     const oai = new OriginAccessIdentity(this, 'OAI');
 
-    // 3. CloudFront distribution
+    // 3. Attach an S3 Bucket Policy that allows CloudFront OAI to perform s3:GetObject
+    const bucketPolicy = new BucketPolicy(this, 'BucketPolicy', {
+      bucket: websiteBucket,
+    });
+
+    bucketPolicy.document.addStatements(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [`${websiteBucket.bucketArn}/*`],
+        principals: [
+          new iam.CanonicalUserPrincipal(oai.cloudFrontOriginAccessIdentityS3CanonicalUserId),
+        ],
+      })
+    );
+
+    // 4. Create CloudFront Distribution with S3Origin using the OAI
     const distribution = new Distribution(this, 'MyDistribution', {
       defaultBehavior: {
         origin: new S3Origin(websiteBucket, {
@@ -39,12 +48,18 @@ export class SdkInfraStack extends Stack {
       defaultRootObject: 'index.html',
     });
 
-    // 4. Deploy 'dist/' folder from React to S3
+    // 5. Deploy the contents of the "dist" folder from your React build to the S3 bucket
     new BucketDeployment(this, 'DeployWebsite', {
-      sources: [Source.asset('../dist')], // path to the dist folder
+      sources: [Source.asset('../dist')],
       destinationBucket: websiteBucket,
       distribution,
-      distributionPaths: ['/*'], // Invalidate everything after upload
+      distributionPaths: ['/*'], // Invalidate CloudFront cache after deployment
+    });
+
+    // 6. (Optional) Output the CloudFront Distribution Domain
+    new CfnOutput(this, 'CloudFrontURL', {
+      value: distribution.distributionDomainName,
+      description: 'The domain name of the CloudFront distribution',
     });
   }
 }
